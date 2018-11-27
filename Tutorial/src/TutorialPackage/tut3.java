69cbde590a9a8bd5cd2858095528098564d0de66
liannep1 <liannep1@091DS-1182-15.student.cssd.ab.ca>
2018-11-27 12:11:15 -0700

    g

--- Tutorial/src/TutorialPackage/tut3.java
+++ Tutorial/src/TutorialPackage/tut3.java
@@ -1,36 +1,9 @@
 package TutorialPackage;
-import java.util.ArrayList;
-public class tut3 { //ArrayListTest
+public class tut3 { 
 	
 	public static void main(String[] args) {
-		
-		ArrayList<Integer> listOfInts = new ArrayList<>();
-		//methods of ArrayLists
-		
-		listOfInts.add(98);//adding to ArrayList
-		listOfInts.add(55);
-		listOfInts.add(1,80);//inserts to index 1 in this case
-		listOfInts.remove(0);//remove item at first index
-		listOfInts.get(0);//returns item at index 0
-		listOfInts.size();//returns size of ArrayList
-		
-		//print ArrayList
-		listOfInts.add(98);
-		listOfInts.add(24);
-
-		for(int i = 0; i < listOfInts.size(); i++) {
-			System.out.println(listOfInts.get(i));
-		}
-		
-		listOfInts.remove(listOfInts.size()-1);//remove last element
-		//print new ArrayList
-		for(int i = 0; i < listOfInts.size(); i++) {
-			System.out.println(listOfInts.get(i));
-
-		}
-		
-		System.out.println(listOfInts.indexOf(98));
-		System.out.println(listOfInts.indexOf(99));
+		int integer = 876;
+		System.out.println(integer);
 
 	}//end main
 	
--- Tutorial/src/TutorialPackage/tut4.java
+++ Tutorial/src/TutorialPackage/tut4.java
@@ -4,50 +4,7 @@ public class tut4 {//TicTacToe
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] grid = new String[3];//single dimension array
-		grid[0] = "-";
-		grid[1] = "-";
-		grid[2] = "-";
-		
-		String[][] board = new String[3][3];//create 2D array
-		board[0][0] = "-";
-		board[0][1] = "-";
-		board[0][2] = "-";
-
-		for(int i = 0; i < grid.length; i++) {
-			System.out.println(grid[i]);
-		}
-		
-		//fill and print 2D array
-		System.out.println("fill and 2D array");
-		for(int row = 0; row < board.length; row++) {
-			//prints each column
-			for(int col = 0; col < board[row].length; col++) {
-				board[row][col] = " - ";
-				System.out.println(board[row][col]);
-			}
-			
-			System.out.println();//prints a new line after the row
-
-		}//end for loop to print rows
-		
-		board[1][1] = " X ";
-		
-		for(int row = 0; row < board.length; row++) {
-			//prints each column
-			for(int col = 0; col < board[row].length; col++) {
-//			board[row][col] = "-";
-				System.out.print(board[row][col]);
-			}
-	
-			System.out.println();
-
-		}//end for
 
 	}//end main
-	
-//int player = 1;
-//take input player
-//player = 2;
 
 }//end class
