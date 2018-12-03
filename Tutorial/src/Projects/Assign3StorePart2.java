91804744c948f144c5422e2577f72567ce3a6986
liannep1 <liannep1@091DS-1182-15.student.cssd.ab.ca>
2018-12-03 12:23:18 -0700

    data stuctures

--- Tutorial/src/Projects/Assign3StorePart2.java
+++ Tutorial/src/Projects/Assign3StorePart2.java
@@ -8,8 +8,14 @@ public class Assign3StorePart2 {
 		Scanner input = new Scanner(System.in);//creates a scanner to scan input
 		
 //a)		
-		String[] products = {"bananas","","","","","","","","",""};
+		String[] products = {"banana","yogurt","cheese","lettuce","cucumber","tomato","fish","beef","orange","pineapple"};
+		double[] price = {0.80,3.99,4.99,2.35,2.46,4.65,7.54,10.56,4.61,5.49};
+		int totalQty = 0;
+		
+		for(int i = 0; i < products.length; i++) {
+			System.out.println((i+1) + products[i]);
+		}
+ 
+	}//end main
 
-	}
-
-}
+}//end class
--- Tutorial/src/TutorialPackage/tut3.java
+++ Tutorial/src/TutorialPackage/tut3.java
@@ -1,10 +1,30 @@
 package TutorialPackage;
 public class tut3 { 
 	
+	public static boolean doesMatch() {
+	
+		boolean match;
+		boolean d = true;
+		boolean f = true;
+		
+		if(d == f) {
+			match = true;
+		}
+		else {
+			match = false;
+		}
+		
+		System.out.println(match);	
+		return doesMatch();
+		
+	}//end doesMatch
+	
 	public static void main(String[] args) {
-		int integer = 876;
-		System.out.println(integer);
+		
+		doesMatch();
 
 	}//end main
 	
 }//end class
+
+
