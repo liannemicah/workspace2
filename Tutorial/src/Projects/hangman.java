65488711ba84c912d75b05a0d49bf0ff0d6854c6
liannep1 <liannep1@091DS-1182-15.student.cssd.ab.ca>
2019-01-10 11:22:27 -0700

    g

---
+++ Tutorial/src/Projects/hangman.java
@@ -0,0 +1,11 @@
+package Projects;
+
+public class hangman {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double 
+
+	}
+
+}
---
+++ Tutorial/src/Square/SquareTester.java
@@ -0,0 +1,17 @@
+/**
+ * A tester for the Square class
+ * You don't need to change or modify this file   
+ */
+package Square;
+
+public class SquareTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    Square square = new Square(10.4);
+	    System.out.println(square.getSide());
+	    System.out.println("Expected: 10.4");
+
+	}
+
+}
