aa97c66c991e6c2ca573f887fa7c910db0b581dc
liannep1 <liannep1@091DS-1182-15.student.cssd.ab.ca>
2019-01-10 12:11:18 -0700

    hgv

--- Tutorial/src/TutorialPackage/tut3.java
+++ Tutorial/src/TutorialPackage/tut3.java
@@ -1,10 +1,22 @@
 package TutorialPackage;
 
 public class tut3 { 
+	
+	public Car()
+	{
+		
+	}
 
 	public static void main(String[] args) {
+		Car mazda = new Car(35);
+	       
 
-
+		// TODO: add your code to add 20 gallons to the tank, drive 105 miles, and add 5 gallons
+	 
+	       
+		//Don't need to modify the line below
+	    System.out.println(mazda.getGasInTank());
+	    System.out.println("Expected: 22.0");
 
 	}//end main
 		
