c40346ef46682a236590ec400bd3e7130a98ba6c
liannep1 <liannep1@091DS-1182-15.student.cssd.ab.ca>
2019-01-18 11:58:35 -0700

    gf

--- Tutorial/src/TutorialPackage/tut3.java
+++ Tutorial/src/TutorialPackage/tut3.java
@@ -2,23 +2,32 @@ package TutorialPackage;
 
 public class tut3 { 
 	
-	public Car()
-	{
-		
-	}
+    private double milesDriven;
+    private double gasInTank;
+    
+    // public void drive(double distance)
+    // {
+    //     milesDriven = milesDriven + distance;
+    // }
+    
+    // public void addGas(double amount)
+    // {
+    //     gasInTank = gasInTank + amount;
+    // }
 
-	public static void main(String[] args) {
-		Car mazda = new Car(35);
-	       
+   public static void main(String[] args)
+   {
+       Car mazda = new Car(35);
+       mazda.drive(105);
+       mazda.addGas(25);
 
-		// TODO: add your code to add 20 gallons to the tank, drive 105 miles, and add 5 gallons
-	 
-	       
-		//Don't need to modify the line below
-	    System.out.println(mazda.getGasInTank());
-	    System.out.println("Expected: 22.0");
-
-	}//end main
+       // TODO: add your code to add 20 gallons to the tank, drive 105 miles, and add 5 gallons
+ 
+       
+       //Don't need to modify the line below
+       System.out.println(mazda.getGasInTank());
+       System.out.println("Expected: 22.0");
+   }
 		
 }//end class
 
